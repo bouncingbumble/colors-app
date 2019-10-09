17d8e759dc3648a3e1ae93e7271d2d3fc99c80d0
Jordan Boudreau <jordboudreau@gmail.com>
2019-10-08 21:10:32 -0700

    add logic to generate scaling colors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Pallete from './Pallete';
 import seedColors from './seedColors'
+import { generatePalette } from './colorHelpers'
 
 function App() {
-  return (
-    <div className="App">
-        <Pallete {...seedColors[4]}></Pallete>
-    </div>
-  );
+    console.log(generatePalette(seedColors[4]))
+    return (
+        <div className="App">
+            <Pallete {...seedColors[4]}></Pallete>
+        </div>
+    );
 }
 
 export default App;
