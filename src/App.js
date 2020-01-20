48050ffc55442dfddedab66d4833d51a215a089b
Jordan Boudreau <jordboudreau@gmail.com>
2020-01-20 10:14:09 -0800

    add new plaette form and link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import seedColors from './seedColors'
 import { generatePalette } from './colorHelpers'
 import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette';
+import NewPaletteForm from './NewPaletteForm'
 
 function App() {
     const findPalette = id => {
@@ -13,6 +14,7 @@ function App() {
     return (
         <Switch>
             <Route exact path="/" render={routeProps => <PaletteList {...routeProps} palettes={seedColors} />} />
+            <Route exact path="/palette/new" render={() => <NewPaletteForm />} />
             <Route
                 exact
                 path="/palette/:id"
