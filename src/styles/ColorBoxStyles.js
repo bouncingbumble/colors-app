ac8f7f09697c0aa2c85286b80ce0fe87aa78927d
Jordan Boudreau <jordboudreau@gmail.com>
2020-04-15 17:15:01 -0700

    make color box responsve

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -19,7 +19,7 @@ export default {
         [sizes.down("lg")]: {
             width: "25%",
             height: props =>
-                props.showingFullPalette ? "20%" : "40%",
+                props.showingFullPalette ? "20%" : "33.3333%",
         },
         [sizes.down("md")]: {
             width: "50%",
@@ -123,6 +123,9 @@ export default {
             marginBottom: "0",
             padding: "1rem",
             textTransform: "uppercase",
+            [sizes.down("xs")]: {
+                fontSize: "6rem"
+            },
         },
         "& p": {
             fontSize: "2rem",
